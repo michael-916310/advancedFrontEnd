ff19f8b20e99f3ec855802127db7b2e86cbca1c6
michael-916310 <916310@rambler.ru>
2023-05-21 16:14:42 +0400

    3.23 Анализ размера банда. BundleAnalyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true }],
-    'max-len': ['error', { ignoreComments: true, ignorePattern: 'import*' }],
+    'max-len': ['error', { ignoreComments: true, ignorePattern: 'import*', code: 100 }],
   },
   globals: {
     __IS_DEV__: true,
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 function BuildPlugins({
@@ -20,6 +21,9 @@ function BuildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }),
   ];
 }
 
